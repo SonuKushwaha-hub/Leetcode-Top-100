1bdfcc2df9006590b7798c3a72bafdfe04001a0c
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-02-08 14:38:13 +0530

    problem-11

---
+++ ContainerWithMostWater.js
@@ -0,0 +1,15 @@
+var maxArea = function (height) {
+  let maxi = 0, i = 0, j = height.length - 1;
+
+  // concept is simply to find out the taller pillers farther to maximize the area
+  while (i < j) {
+      maxi = Math.max(maxi, Math.min(height[i], height[j]) * (j - i));
+      if (height[i] < height[j]) i++;
+      else j--;
+  }
+  return maxi;
+};
+
+// test fo code
+const height = [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(height));
