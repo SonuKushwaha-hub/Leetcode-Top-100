e7aec9a5b415161621f1e790108b78c6ec043e76
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-02-08 14:04:11 +0530

    problme-9

---
+++ PalindromeNumber.js
@@ -0,0 +1,13 @@
+var isPalindrome = function(x) {
+  // convert to string and to array of characters
+  let arr = x.toString().split("");
+
+  for(let i=0;i<arr.length/2;i++){
+      if(arr[i] !== arr[arr.length-1-i]) return false;
+  }
+  return true;
+};
+
+// testing code
+const x = 121;
+console.log(isPalindrome(x));
