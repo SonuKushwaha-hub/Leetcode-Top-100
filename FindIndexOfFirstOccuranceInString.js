6a363c68e96ef10f07bb2a2dbd4bf504362d2a0f
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-02-12 12:17:44 +0530

    problme-28

---
+++ FindIndexOfFirstOccuranceInString.js
@@ -0,0 +1,7 @@
+var strStr = function (haystack, needle) {
+  // method indexOf in String satisfies the required criteria
+  return haystack.indexOf(needle);
+};
+
+// test code
+console.log(strStr("sadbutsad","sad"));
