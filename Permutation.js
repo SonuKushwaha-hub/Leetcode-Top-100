94b0c3ab00477b8c62b84b676a6b4041e71a73d5
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-02-15 14:53:53 +0530

    problem-46 revised

--- Permutation.js
+++ Permutation.js
@@ -1,28 +1,23 @@
-var func = (id, nums, ans) => {
-  // initial return case in recursion
-  if (id === nums.length) {
-    ans.push(nums);
-    console.log(nums);
-    return;
-  }
-
-  // resursion loop
-  for (let i = id; i < nums.length; i++) {
-    // swap element-i with element-id
-    [nums[i], nums[id]] = [nums[id], nums[i]];
-    // call recursion on next id+1
-    func(id + 1, nums, ans);
-    // swap back the elements
-    [nums[i], nums[id]] = [nums[id], nums[i]];
-  }
-}
-
 var permute = function (nums) {
-  let ans = new Array();
-  // call recursion function
-  func(0, nums, ans);
-  return ans;
+  let res = []
+
+  // define fuction to call recursively
+  let iterate = (arr, temp) => {
+    // initial retun case
+    if (arr.length == 0) {
+      res.push(temp)
+      return;
+    }
+    for (let i = 0; i < arr.length; i++) {
+      // use filter to filter the non-identical element and add it to temp
+      iterate(arr.filter((num, idx) => idx != i), [...temp, arr[i]])
+    }
+  }
+
+  // call the recursive function
+  iterate(nums, [])
+  return res
 };
 
 // test code
-permute([1,2,3]);
+console.log(permute([1, 2, 3]));
